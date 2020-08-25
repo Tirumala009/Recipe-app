6f48d25af7d8d03a3514aa554982cd7275c66c67
Tirumala009 <69470807+Tirumala009@users.noreply.github.com>
2020-08-25 18:34:16 +0100

    Updates

---
+++ precache-manifest.ce49fea44522fdcf344ef2dd97fd8ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906381f02a853e0a5d7406f79574f3ff",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "7cbe8300c773b909170f",
+    "url": "/recipe-app/static/css/main.4f1f5886.chunk.css"
+  },
+  {
+    "revision": "af5a48456ec6fa3bf301",
+    "url": "/recipe-app/static/js/2.b611695f.chunk.js"
+  },
+  {
+    "revision": "f3e4988b51540f436f9a94346f1ce57f",
+    "url": "/recipe-app/static/js/2.b611695f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cbe8300c773b909170f",
+    "url": "/recipe-app/static/js/main.5713550b.chunk.js"
+  },
+  {
+    "revision": "95f8ec566e7199ca77cb",
+    "url": "/recipe-app/static/js/runtime-main.5e04f524.js"
+  }
+]);
